202aa4fb6d85b4c7f486ca4de5d11f0a278c02de
Vinicius Duarte Galdino <vduarteg@everis.com>
2019-02-27 10:53:24 -0300

    fix securitu

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,16 +1,16 @@
-var mysql = require('mysql');
-var host = "172.17.0.2";
-var pass = "123456"
+// var mysql = require('mysql');
+// var host = "172.17.0.2";
+// var pass = "123456"
 
-var connMySQL = function(){
-	return mysql.createConnection({
-		host : host,
-		user : 'root',
-		password : pass,
-		database : 'kaos'
-	});
-}
+// var connMySQL = function(){
+// 	return mysql.createConnection({
+// 		host : host,
+// 		user : 'root',
+// 		password : pass,
+// 		database : 'kaos'
+// 	});
+// }
 
-module.exports = function () {
-	return connMySQL;
-}
+// module.exports = function () {
+// 	return connMySQL;
+// }
